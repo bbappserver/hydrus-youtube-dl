2938ea9df95cce789f7db5785d84f8803121a3b9
bbserve <bbappserver2@gmail.com>
2019-04-13 23:30:41 -0700

    Added more setup stuff to suck less but still broken until I care to look how that works.

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-install_requires=['hydrus @ https://gitlab.com/cryzed/hydrus-api/-/archive/master/hydrus-api-master.zip']
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+setup(
+    name="HelloWorld",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['hydrus @ https://gitlab.com/cryzed/hydrus-api/-/archive/master/hydrus-api-master.zip']
+)
